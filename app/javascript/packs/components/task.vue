dc037cac64ef969160efa23cdad5940e2e5feec7
lucassherwin <lksherwin@gmail.com>
2021-08-06 13:55:47 -0400

    icons now showup

--- app/javascript/packs/components/task.vue
+++ app/javascript/packs/components/task.vue
@@ -1,54 +1,71 @@
 <template>
-  <v-list
-    flat
-    subheader
-    three-line
+  <v-data-table
+    v-model="selected"
+    :headers="headers"
+    :items="desserts"
+    item-key="name"
+    show-select
+    @click:row="handleClick"
+    class="elevation-1"
   >
-  <v-subheader>General</v-subheader>
-
-    <v-list-item-group
-      v-model="settings"
-      multiple
-      active-class=""
-    >
-      <v-list-item>
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox :input-value="active"></v-checkbox>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title>Notifications</v-list-item-title>
-            <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
-
-      <v-list-item>
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox :input-value="active"></v-checkbox>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title>Sound</v-list-item-title>
-            <v-list-item-subtitle>Auto-update apps at any time. Data charges may apply</v-list-item-subtitle>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
-
-      <v-list-item>
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox :input-value="active"></v-checkbox>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title>Auto-add widgets</v-list-item-title>
-            <v-list-item-subtitle>Automatically add home screen widgets when downloads complete</v-list-item-subtitle>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
-    </v-list-item-group>
-  </v-list>
+  </v-data-table>
 </template>
+
+<script>
+  export default {
+    methods:{
+      handleClick(value)
+      {
+        // do a put and update that specific item in the backend
+        console.log(value)
+      }
+    },
+
+    data () {
+      return {
+        singleSelect: false,
+        selected: [],
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+        ],
+      }
+    }
+  }
+
+</script>
